b6fdd8c86652e401e63fd0e57534ffc2a9b81edb
Dario Graf <dario.graf@comvation.com>
2024-06-09 21:15:16 +0200

    feat: add responsive changes

--- js/index.js
+++ js/index.js
@@ -76,10 +76,31 @@ document.addEventListener('DOMContentLoaded', function () {
         customCursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
     }
 
-    document.addEventListener('mousemove', function (e) {
-        requestAnimationFrame(function () {
-            moveCursor(e);
+    // Function to check if the device is mobile
+    function isMobile() {
+        return window.innerWidth <= 768; // Adjust this value if necessary
+    }
+
+    // Add the event listeners only if the device is not mobile
+    if (!isMobile()) {
+        document.addEventListener('mousemove', function (e) {
+            requestAnimationFrame(function () {
+                moveCursor(e);
+            });
         });
+    }
+
+    // Optional: Handle window resize events to add/remove event listeners dynamically
+    window.addEventListener('resize', function () {
+        if (isMobile()) {
+            document.removeEventListener('mousemove', moveCursor);
+        } else {
+            document.addEventListener('mousemove', function (e) {
+                requestAnimationFrame(function () {
+                    moveCursor(e);
+                });
+            });
+        }
     });
 });
 
